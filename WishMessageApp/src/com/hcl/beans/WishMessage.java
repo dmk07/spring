30ca302a59d080ddde852ad62fab96b69f126e1f
dmk61 <dmk61@Dipak-Kadam>
2023-07-18 02:00:37 +0530

    issue with reloading xml file

---
+++ WishMessageApp/src/com/hcl/beans/WishMessage.java
@@ -0,0 +1,20 @@
+package com.hcl.beans;
+
+import java.util.Date;
+
+public class WishMessage {
+
+	private Date date;
+
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	public String Wish(String user)
+	{
+		return user;
+	}
+	
+}
---
+++ WishMessageApp/src/com/hcl/test/TestApp.java
@@ -0,0 +1,9 @@
+package com.hcl.test;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
