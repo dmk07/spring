7aa2cb68e65987757f570c95f8234491ec1d263e
dmk61 <dmk61@Dipak-Kadam>
2023-07-18 02:10:50 +0530

    FileSystemResource class issue is resolved. issue was forgotton to add spring core jar files

--- WishMessageApp/src/com/hcl/beans/WishMessage.java
+++ WishMessageApp/src/com/hcl/beans/WishMessage.java
@@ -14,7 +14,20 @@ public class WishMessage {
 	
 	public String Wish(String user)
 	{
-		return user;
+		int hour=date.getHours();
+		
+		if(hour<=12)
+		{
+			return "Good Morning"+user;
+					
+		}else if(hour<=16)
+		{
+			return "Good Afternoon"+user;
+		}else if(hour<=20)
+			return "Good Night"+user;
+		else
+			throw new IllegalArgumentException(); 
+		
 	}
 	
 }
--- WishMessageApp/src/com/hcl/test/TestApp.java
+++ WishMessageApp/src/com/hcl/test/TestApp.java
@@ -1,8 +1,24 @@
 package com.hcl.test;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+
+import com.hcl.beans.WishMessage;
+
 public class TestApp {
 
 	public static void main(String[] args) {
+	
+		FileSystemResource file=new FileSystemResource("src/com/hcl/config/Config.xml");
+	
+		XmlBeanFactory bean=new XmlBeanFactory(file);
+		
+		Object obj=bean.getBean("wm");
+		
+		WishMessage wm=(WishMessage)obj;
+		String msg=wm.Wish("Dipak");
+		
+		System.out.print(msg);
 	
 	}
 
