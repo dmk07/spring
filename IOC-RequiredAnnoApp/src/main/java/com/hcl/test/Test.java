94576f5f18c81cea67c53c52598e53f38ccbae2c
dmk61 <dmk61@Dipak-Kadam>
2023-09-27 00:46:31 +0530

    normal

--- IOC-RequiredAnnoApp/src/main/java/com/hcl/test/Test.java
+++ IOC-RequiredAnnoApp/src/main/java/com/hcl/test/Test.java
@@ -10,13 +10,12 @@ public class Test {
 
 		ClassPathXmlApplicationContext cx = new ClassPathXmlApplicationContext("com/hcl/config/config.xml");
 
-		Robot robo1= cx.getBean("com.hcl.beans.Robot#1", Robot.class);
-		Robot robo2= cx.getBean("com.hcl.beans.Robot#2", Robot.class);
-
+		Robot robo= cx.getBean("robo", Robot.class);
+	
 		//#n index based ranking
 		
-		System.out.println(robo1);
-		System.out.println(robo2);
+		System.out.println(robo);
+		
 
 	}
 
