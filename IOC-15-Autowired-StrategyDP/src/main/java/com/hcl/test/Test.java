19e86cfecb0fb443616ae324b59eb57fb8bfb9e5
dmk61 <dmk61@Dipak-Kadam>
2023-09-27 02:12:55 +0530

    autowiring-strategy-design-pattern

---
+++ IOC-15-Autowired-StrategyDP/src/main/java/com/hcl/test/Test.java
@@ -0,0 +1,21 @@
+package com.hcl.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.comp.Flipkart;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/hcl/config/config.xml");
+		// get Target class object
+		Flipkart fpkt = ctx.getBean("fpkt", Flipkart.class);
+		// invoke the b.method
+		String result = fpkt.shopping(new String[] { "mangoes", "tamarind", "sugar canes" },
+				new float[] { 900.0f, 300.0f, 100.0f });
+		System.out.println(result);
+
+	}
+
+}
---
+++ IOC-15-Autowired-StrategyDP/src/main/java/com/nt/comp/Flipkart.java
@@ -0,0 +1,50 @@
+package com.nt.comp;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public final class Flipkart {
+	
+	@Autowired
+	@Qualifier("fEx")
+	private  Courier courier;
+	
+/*	@Autowired
+	public Flipkart(@Qualifier("dtdc") Courier courier) {
+		this.courier=courier;
+		System.out.println("Flipkart:: 1-param constructor");
+	}
+	
+	@Autowired
+	@Qualifier("fFlight")
+	public  void   assign(Courier courier) {
+		System.out.println("Flipkart.assign(-)");
+		this.courier=courier;
+	}
+	
+	@Autowired
+	@Qualifier("bDart")
+	public void setCourier(Courier courier) {
+		System.out.println("Flipkart.setCourier(-)");
+		 this.courier=courier;
+	} */
+	
+	
+	
+	//b,method
+	public  String  shopping(String [] items, float[] prices) {
+		float billAmt=0.0f;
+		for(float p:prices)
+			billAmt+=p;  // billAmt=billAmt+p
+		//generate order id
+		  int oid=new Random().nextInt(1000);
+		  //use courier
+		  String msg=courier.deliver(oid);
+		  return  Arrays.toString(items)+" with prices"+Arrays.toString(prices)+" having billamt"+billAmt +" :::: "+ msg;
+	}
+	
+
+}
