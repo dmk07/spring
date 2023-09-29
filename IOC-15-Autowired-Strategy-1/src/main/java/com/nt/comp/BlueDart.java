966c92ae4c8d7f4ea55c0b9723f862e878719007
dmk61 <dmk61@Dipak-Kadam>
2023-09-29 16:43:12 +0530

    @Componenet("dtdc"); @Autowired <context:annotation-config/>

---
+++ IOC-15-Autowired-Strategy-1/src/main/java/com/nt/comp/BlueDart.java
@@ -0,0 +1,19 @@
+package com.nt.comp;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component("bDart")
+@Lazy
+public final class BlueDart implements Courier {
+	
+	public BlueDart() {
+		System.out.println("BlueDart:: 0-param constructor");
+	}
+
+	@Override
+	public String deliver(int oid) {
+         return  "Delivering "+oid+" order Id  order products using BlueDart ";
+	}
+
+}
