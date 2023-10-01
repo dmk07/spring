ae9e9849316d6a0804497398015aa0e3605242cd
dmk61 <dmk61@Dipak-Kadam>
2023-10-02 02:06:01 +0530

    shoppingApp-2.2-@Value

---
+++ ShoppingApp-@value-2.2/src/main/java/com/hcl/comp/Amazon.java
@@ -0,0 +1,32 @@
+package com.hcl.comp;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("amzn")
+public class Amazon implements Shopping {
+
+	@Autowired
+	@Qualifier("fedfx")
+	private Courier courier;
+	
+	
+	@Override
+	public String buy(String[] items, float[] prices) {
+		
+			float billAmt=0.0f;
+			for(float p:prices)
+				billAmt+=p;  // billAmt=billAmt+p
+			//generate order id
+			  int oid=new Random().nextInt(1000);
+			  //use courier
+			  String msg=courier.delivery(oid);
+			  return  Arrays.toString(items)+" with prices"+Arrays.toString(prices)+" having billamt"+billAmt +" :::: "+ msg;
+		}
+	}
+
+
---
+++ ShoppingApp-@value-2.2/src/main/java/com/hcl/comp/Shopping.java
@@ -0,0 +1,7 @@
+package com.hcl.comp;
+
+public interface Shopping {
+	
+	public String buy(String [] items, float[] prices);
+
+}
