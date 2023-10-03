c384c46f6ccab43298c12fa350ce75ab6ae07b8b
dmk61 <dmk61@Dipak-Kadam>
2023-10-04 01:02:55 +0530

    1 day-project: Hostel Management1.0

---
+++ HostelManagement/src/main/java/com/hcl/controller/MainController.java
@@ -0,0 +1,5 @@
+package com.hcl.controller;
+
+public class MainController {
+
+}
---
+++ HostelManagement/src/main/java/com/hcl/dao/StudentDAO.java
@@ -0,0 +1,10 @@
+package com.hcl.dao;
+
+import com.hcl.bo.StudentBO;
+
+public interface StudentDAO {
+	
+	public int insert(StudentBO bo) throws Exception;
+
+
+}
