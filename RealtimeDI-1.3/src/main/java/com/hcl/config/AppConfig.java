eac6430a35139f95183f19323c5671737e07b28c
dmk61 <dmk61@Dipak-Kadam>
2023-10-08 01:58:36 +0530

    providing values from properties file

--- RealtimeDI-1.3/src/main/java/com/hcl/config/AppConfig.java
+++ RealtimeDI-1.3/src/main/java/com/hcl/config/AppConfig.java
@@ -1,24 +1,32 @@
 package com.hcl.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ComponentScan(basePackages="com.hcl")
+@PropertySource(value="com/hcl/comp/info.properties")
 public class AppConfig {
 
+	@Autowired
+	private Environment env;
+	
+	
 	@Bean(name="oracleDS")
 	public DriverManagerDataSource createDataSource()
 	{
 		
-		DriverManagerDataSource ds=new DriverManagerDataSource();
-		ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		ds.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
-		ds.setUsername("system");
-		ds.setPassword("dipak");
-		return ds;
+		DriverManagerDataSource drds=new DriverManagerDataSource();
+		drds.setDriverClassName(env.getRequiredProperty("jdbc.driver"));
+		drds.setUrl(env.getRequiredProperty("jdbc.url"));
+		drds.setUsername(env.getProperty("jdbc.username"));
+		drds.setPassword(env.getProperty("jdbc.password"));
+		return drds;
 		
 	}
 	
